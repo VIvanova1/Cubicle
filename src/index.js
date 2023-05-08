6fa456acc1127032c516c4558f8093010ba50e89
Veneta Ivanova <i.veneta.v@gmail.com>
2023-05-08 14:19:47 +0300

    add config

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ const setupViewEngine = require('./config/viewEngine');
 const app = express();
 setupViewEngine(app);
 
-app.use(express.static('src/public'))
-app.get('/', (req, res) => {
-    res.render('home');
-});
+app.use(express.static('src/public'));
 
-app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`))
+app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
