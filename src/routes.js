e9f8923fb7aa6e22199bf1162fe2305252cd02e6
Veneta Ivanova <i.veneta.v@gmail.com>
2023-05-13 14:20:59 +0300

    details page

--- src/routes.js
+++ src/routes.js
@@ -7,7 +7,7 @@ router.get('/', homeController.getHomePage);
 router.get('/about', homeController.getAboutPage);
 router.get('/create', cubeController.getCreateCube);
 router.post('/create', cubeController.postCreateCube)
-
+router.get('/details/:cubeId', cubeController.getDetails)
 
 
 module.exports = router;
